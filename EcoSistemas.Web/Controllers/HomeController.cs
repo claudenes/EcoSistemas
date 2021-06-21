16abad60566502c2a9bc0adb7b09b597c2449641
ClaudenesAlmeida <claudenes@gmail.com>
2021-06-21 13:54:43 -0300

    #passo 2

--- EcoSistemas.Web/Controllers/HomeController.cs
+++ EcoSistemas.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -25,20 +26,11 @@ namespace EcoSistemas.Web.Controllers
         public IActionResult Index([FromServices] IConfiguration config)
         {
             var token = GetToken(config);
-
+            Token viewDataVariavel = ViewData["Token"] as Token;
+            var paciente = GetPaciente(config, viewDataVariavel);
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
         public IActionResult GetToken([FromServices] IConfiguration config)
         {
             using (var client = new HttpClient())
@@ -62,10 +54,42 @@ namespace EcoSistemas.Web.Controllers
                 token.proximaEtapa = resultado.proximaEtapa;
                 token.statusCode = resultado.statusCode;
 
-                TempData["Token"] = token;
+                ViewData["Token"] = token;
 
 
                 return View(token);
+
+            }
+        }
+        public IActionResult GetPaciente([FromServices] IConfiguration config,Token token)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string baseURL = config.GetSection("EcoSistemas_API:BaseURL").Value;
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.data);
+                HttpResponseMessage response = client.GetAsync(baseURL + "api/Desafio/GetPaciente").Result;
+                response.EnsureSuccessStatusCode();
+
+                string conteudo = response.Content.ReadAsStringAsync().Result;
+
+                dynamic resultado = JsonConvert.DeserializeObject(conteudo);
+
+                dynamic dados = resultado.data;
+
+                Paciente paciente = new Paciente();
+                paciente.usu_nome = dados.usu_nome;
+                paciente.email = dados.email;
+                paciente.telefone = dados.telefone;
+                paciente.sexo = dados.sexo;
+                paciente.bairro = dados.bairro;
+
+                ViewData["Paciente"] = paciente;
+
+                return View(paciente);
 
             }
         }
---
+++ EcoSistemas.Web/Models/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcoSistemas.Web.Models
+{
+    public class Paciente
+    {
+        public string usu_nome { get; set; }
+
+        public string email { get; set; }
+
+        public string telefone { get; set; }
+
+        public string sexo { get; set; }
+
+        public string bairro { get; set; }
+    }
+}
