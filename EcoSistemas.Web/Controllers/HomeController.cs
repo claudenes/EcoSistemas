8a9fbdd36dde3bbcdd81cc54c9f7179f442e6490
ClaudenesAlmeida <claudenes@gmail.com>
2021-06-21 11:12:57 -0300

    #passo 1

---
+++ EcoSistemas.Web/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using EcoSistemas.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace EcoSistemas.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index([FromServices] IConfiguration config)
+        {
+            var token = GetToken(config);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult GetToken([FromServices] IConfiguration config)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string baseURL = config.GetSection("EcoSistemas_API:BaseURL").Value;
+
+                HttpResponseMessage response = client.GetAsync(baseURL + "api/Desafio/GetToken").Result;
+
+                response.EnsureSuccessStatusCode();
+                string conteudo = response.Content.ReadAsStringAsync().Result;
+
+                dynamic resultado = JsonConvert.DeserializeObject(conteudo);
+
+                Token token = new Token();
+                token.data = resultado.data;
+                token.mensagem = resultado.mensagem;
+                token.proximaEtapa = resultado.proximaEtapa;
+                token.statusCode = resultado.statusCode;
+
+                TempData["Token"] = token;
+
+
+                return View(token);
+
+            }
+        }
+    }
+}
---
+++ EcoSistemas.Web/Models/Token.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcoSistemas.Web.Models
+{
+    public class Token
+    {
+        public string mensagem { get; set; }
+
+        public string proximaEtapa { get; set; }
+
+        public string statusCode { get; set; }
+
+        public string data { get; set; }
+    }
+}
